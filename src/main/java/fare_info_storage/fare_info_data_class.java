f60704309d7e2244e7f1e7dd3060ef0254fb6176
jjyo123456 <157724791+jjyo123456@users.noreply.github.com>
2025-08-22 18:00:18 +0530

    Update fare_info_data_class.java
    completed the data class for the fare info storage

--- src/main/java/fare_info_storage/fare_info_data_class.java
+++ src/main/java/fare_info_storage/fare_info_data_class.java
@@ -1,11 +1,24 @@
-public class fare_info_datA_CLASS{
+public class fare_info_data_class{
     @Id 
     @AutoGennerate(GennerationType = Gennerate.UNQIUE)
-    private int id;
+    @SqeyenceGennerator(sequence_name = "fare_id_sequence" table_name = "fare_id_sequence" Allocationsize = "1")
+    private String fare_id;
 
-    private String auto_id;
-    private String auto_driver_name;
-    private Double fare;
-    private TimeStamp time;
+    private String driver_id;//to be connected to the driver database
+    private String vehicle_id;// to be connected to the vehicle database
+
+    private TimeStamp start_time;
+    private TimeStamp end_time;
+    private Double distance_km;
+    private Double fare_amount;
+    
+
+    @PrePersist
+    public void prePersist(){
+    if(fare_id == null){
+    this.fare_id = "FARE-" + UUID.randomUUID().toString();
+    }
+    }
+
 
 }
